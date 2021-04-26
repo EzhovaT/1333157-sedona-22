a574fbcf78eda0c7e6e25aa616b46591e6e04643
Tatyana <tatyezhova@yandex.ru>
2021-04-26 17:42:05 +0300

    Декоративные элементы главной страницы.

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,7 @@
 const btn = document.querySelector(".main-nav__toggle"),
   nav = document.querySelector(".main-nav");
 
-  nav.classList.remove('main-nav_no-js')
+nav.classList.remove("main-nav_no-js");
 
 btn.addEventListener("click", () => {
   nav.classList.toggle("main-nav_closed");
