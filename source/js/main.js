e749e099eec837d9eda1674acd7420d1dc4dbc29
Tatyana <tatyezhova@yandex.ru>
2021-05-02 19:57:23 +0300

    Элементы главной страницы

--- source/js/main.js
+++ source/js/main.js
@@ -3,8 +3,8 @@
 const btn = document.querySelector(".main-nav__toggle"),
   nav = document.querySelector(".main-nav");
 
-nav.classList.remove("menu-nojs");
+nav.classList.remove("main-nav_nojs");
 
 btn.addEventListener("click", () => {
-  nav.classList.toggle("closed-menu");
+  nav.classList.toggle("main-nav_closed-menu");
 });
