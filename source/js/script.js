4ed829bb25b02d38caeeed2f25ed238aba72e2f7
Tatyana <tatyezhova@yandex.ru>
2021-05-23 11:42:45 +0300

    Доработка проекта

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,7 @@ const btn = document.querySelector(".main-nav__toggle"),
   nav = document.querySelector(".main-nav");
 
 nav.classList.remove("main-nav_nojs");
+nav.classList.add("main-nav_closed-menu");
 
 btn.addEventListener("click", () => {
   nav.classList.toggle("main-nav_closed-menu");
